14196de2b2c2512f5137db3d6bdf2bad1e44b3f0
fined000@naver.com <fined000@naver.com>
2023-03-19 19:21:29 +0900

    day 1 commit2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,6 @@
-export const trending = (req, res) => res.send(
-  "<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Home</h1><footer>&copy;2023 Youtube -  All rights reserved</footer></body></html>"
-  );
-export const edit = (req, res) => {res.send(
-  `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2023 Wetube -  All rights reserved</footer></body></html>`
-)};
-export const see = (req, res) => {return res.send(
-  `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2023 Wetube -  All rights reserved</footer></body></html>`
-)};
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
+export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {res.send("Delete Video");};
